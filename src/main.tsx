6d1e982e97f23a0357e12383cbd4c70e0fc06f90
eirish <eirishofficial@gmail.com>
2026-01-29 14:01:51 +0100

    missed commit

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,12 @@ import './presentation/styles/components.css';
 import './presentation/styles/navigation.css';
 import './presentation/styles/accessibility.css';
 
+const rawBase = import.meta.env.BASE_URL as string;
+const basename = rawBase.replace(/\/$/, '') || '/';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
