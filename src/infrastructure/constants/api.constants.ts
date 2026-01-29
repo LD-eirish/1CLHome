98e932e5f4bdfea7762213bfdc7551f001d8f472
eirish <eirishofficial@gmail.com>
2026-01-29 13:47:55 +0100

    Update api.constants.ts

--- src/infrastructure/constants/api.constants.ts
+++ src/infrastructure/constants/api.constants.ts
@@ -6,10 +6,10 @@
 import type { CacheConfig, PathConfig } from '../types/common.types';
 
 export const API_ENDPOINTS = {
-  FRAMEWORK_DATA: 'src/data/framework-data.json',
-  RESOLUTIONS: 'src/data/resolutions.json',
-  REGIMENTS: 'src/data/regiments.json',
-  RESOLUTION_TEMPLATE: 'src/data/resolution-template.json'
+  FRAMEWORK_DATA: '/1CLHome/src/data/framework-data.json',
+  RESOLUTIONS: '/1CLHome/src/data/resolutions.json',
+  REGIMENTS: '/1CLHome/src/data/regiments.json',
+  RESOLUTION_TEMPLATE: '/1CLHome/src/data/resolution-template.json'
 } as const;
 
 export const CACHE_CONFIG: CacheConfig = {
